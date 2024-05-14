0ad8380a5e0c5f833ac7ab4e20bcc32c98b786f5
Francisco-int <franciscos1205@gmail.com>
2024-05-14 17:47:51 -0300

    0.12

--- Assets/EnemigoPadre.cs
+++ Assets/EnemigoPadre.cs
@@ -5,18 +5,27 @@ using UnityEngine;
 public abstract class EnemigoPadre : MonoBehaviour
 {
     [SerializeField] float velocidadEnemgio;
+    [SerializeField] float posicionIdle;
+    [SerializeField] float atacarOtraVez;
     [SerializeField] float dañoDelEnemigo;
-    [SerializeField] Animator anim;
+    public Animator anim;
     [SerializeField] Vector2 playerPos;
     [SerializeField] bool irAtacar;
+    [SerializeField] bool Atacar;
+    [SerializeField] bool largaDistancia;
+    [SerializeField] bool cortaDistancia;
     [SerializeField] Vector2 enemigoPos;
     [SerializeField] Jugador jugador;
-
+    [SerializeField] float xJugador;
+    [SerializeField] float yJugador;
+    [SerializeField] float xEnemigo;
+    [SerializeField] float yEnemigo;
 
     // Start is called before the first frame update
     void Start()
     {
-        playerPos = GameObject.Find("Player").GetComponent<Vector2>();
+        jugador = GameObject.Find("Jugador").GetComponent<Jugador>();
+        anim = GetComponent<Animator>();
        
     }
 
@@ -25,28 +34,112 @@ public abstract class EnemigoPadre : MonoBehaviour
     {
         if (irAtacar)
         {
-
+            MovimientoEnemigos();
         }
+        playerPos = jugador.transform.position;
+        enemigoPos = gameObject.transform.position;
+        
     }
 
     void MovimientoEnemigos()
     {
-
-
+        
+        if (playerPos.x > enemigoPos.x)
+        {
+            anim.SetInteger("Walk", 1);
+        }
+        if (playerPos.x < enemigoPos.x)
+        {
+            anim.SetInteger("Walk", 2);
+        }
+        if (playerPos.y > enemigoPos.y)
+        {
+            anim.SetInteger("Walk", 3);
+        }
+        if (playerPos.y < enemigoPos.y)
+        {
+            anim.SetInteger("Walk", 4);
+        }
 
         transform.Translate(playerPos * Time.deltaTime * velocidadEnemgio);
-    }
 
+    }
+    void Ataque()
+    {
+        DirecionAtaque();
+        jugador.GTvidaJugador = dañoDelEnemigo;
+        StartCoroutine(AtaqueCoolDownAnimation());
+    }
+    IEnumerator AtaqueCoolDownAnimation()
+    {
+
+        yield return new WaitForSeconds(posicionIdle);
+
+        anim.SetInteger("Ataque", 0);
+
+        yield return new WaitForSeconds(atacarOtraVez);
+        if (Atacar)
+        {
+            Ataque();
+        }
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.GetComponent<Jugador>())
+        Debug.Log("Colicion");
+        if (collision.gameObject.GetComponent<Jugador>() && largaDistancia!)
         {
-            Jugador jugador = collision.gameObject.GetComponent<Jugador>();
-            jugador.GTvidaJugador = dañoDelEnemigo;
+            irAtacar = false;
+            Atacar = true;
+            Ataque();        
         }
-
     }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        Atacar = false;
+        irAtacar = true;  
+    } 
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        Debug.Log("Trigger");
+        if (collider.gameObject.GetComponent<Jugador>() && largaDistancia!)
+        {
+            irAtacar = true;
+        }
+        if (collider.gameObject.GetComponent<Jugador>() && largaDistancia)
+        {
+            irAtacar = false;
+            Atacar = true;
+            Ataque();       
+        }
+    }
+    void DirecionAtaque()
+    { 
+        xJugador = jugador.transform.position.x;
+        yJugador = jugador.transform.position.y;
+        xEnemigo = gameObject.transform.position.x;
+        yEnemigo = gameObject.transform.position.y;
+
+        if (xJugador > xEnemigo && yJugador - yEnemigo < xJugador - xEnemigo)
+        {
+            anim.SetInteger("Ataque", 1);
+        }
+        if (xJugador < xEnemigo && yJugador - yEnemigo > xJugador - xEnemigo)
+        {
+            anim.SetInteger("Ataque", 2);
+        }
+        if (yJugador > yEnemigo && xJugador - xEnemigo < yJugador - yEnemigo)
+        {
+            anim.SetInteger("Ataque", 3);
+        }
+        if (yJugador < yEnemigo && xJugador - xEnemigo > yJugador - yEnemigo)
+        {
+            anim.SetInteger("Ataque", 4);
+        }
+        Debug.Log("Animacion Ataque");
+        
+    }
+
 
     protected abstract void LanzarBolaFuego();
 
---
+++ Assets/Enemigos/Flam(Lanzador Bolas de fuego)/Flam.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flam : EnemigoPadre
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    protected override void LanzarBolaFuego()
+    {
+        //anim.SetInteger("Ataque", 1)
+    }
+}
