7482a2d9e71bcbd08f41f83cd541efc2efda8207
delfisita <131388243+delfisita@users.noreply.github.com>
2024-05-17 18:42:07 -0300

    0.21

--- Assets/Jugador/ScriptsJugador/PowerUps.cs
+++ Assets/Jugador/ScriptsJugador/PowerUps.cs
@@ -1,21 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public abstract class PowerUps : MonoBehaviour
+public class PowerUps : Jugador
 {
-    Animator animator;
+    int powerUpSelector; //Esto servira para saber cual tipo de powerUp hay que activar
+    [SerializeField] int dañoPowerUp;
+
     void Start()
     {
-        animator = GetComponent<Animator>();
-    }
-   public void Collect()
-    {
 
-        animator.SetTrigger("Collected");
     }
-    public void Dissapear()
+    void Update()
     {
-        Destroy(gameObject);
+        //if(input 1)
+        {
+            //Esto le dara un valor a la variable powerUpSelector 
+        }
+        //if(input 2)
+        {
+            //Esto le dara un valor a la variable powerUpSelector 
+        }
+        //if(input B)
+        {
+            //Esto activara el metodo PowerUp pasandole el valor de powerUpSelector
+        }
+    }
+
+    void PowerUp(int tipoPowerUp)
+    {
+        //if(tipoPowerUp == 1)
+        {
+            //Activar metodo BolaDeFuego
+        }
+        //if(tipoPowerUp == 2)
+        {
+            //Activar metodo PinchoHielo
+        }
+    }
+
+    void BolaDeFuego()
+    {
+        //Se ejecutara la animación y llamara al metodo RecibirDaño del enemigo
+        //pasandole un valor(el daño de la habilidad). Y le eliminara el objeto 
+        //que activa este powerUp del inventario del player.
+    }
+    void PinchoHielo()
+    {
+        //Se ejecutara la animación y llamara al metodo RecibirDaño del enemigo
+        //pasandole un valor(el daño de la habilidad). Y le eliminara el objeto 
+        //que activa este powerUp del inventario del player.
     }
 }
